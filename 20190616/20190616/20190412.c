efe50403fa0db96439efb0ff60db432e9f012936
Gausalc <1178321407@qq.com>
2019-06-17 19:51:48 +0800

    20190412课堂

---
+++ 20190616/20190616/20190412.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main()
+{
+	int age = 0;
+	scanf("%d", &age);
+	printf("age = %d\n", age);
+	system("system");
+
+	return 0;
+}
