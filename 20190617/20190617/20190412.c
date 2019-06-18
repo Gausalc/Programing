02bab55ef1fdb6f0ae5465218bda122650c23f6b
Gausalc <1178321407@qq.com>
2019-06-18 22:53:55 +0800

    20190412学习内容

---
+++ 20190617/20190617/20190412.c
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+
+
+//类型长度
+//int main()  
+//{
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//	printf("%d\n", sizeof(long double));
+//	system("pause");
+//	return 0;
+//}
+
+
+//{}代码块
+//int a = 20;//全局
+//int main() 
+//{ 
+//	int a = 10;//局部
+//	printf("%d\n", a);
+//	system("pause");
+//	return 0;
+//}
+
+
+////两个数相加
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	printf("输入两个数");
+//	scanf("%d%d\n", &num1, &num2);
+//	sum = num1 + num2;
+//	printf("sum=%d\n", sum);
+//	system("pause");
+//	return 0;
+//		
+//} 
+
+
+//printf转义字符
+//int main()
+//{
+//	printf("\t
+//}
+
+
+
